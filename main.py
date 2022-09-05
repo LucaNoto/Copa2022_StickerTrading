e5ac0a4d1cf90d20db693e5821f088a4d29f3437
LucaNoto <luca.ribeiro.noto@gmail.com>
2022-09-05 12:54:07 -0300

    Create main.py
    Defines parameters and invokes function from function.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from function import FindSwappables
+
+myNeed = ""
+myRepeated = ""
+peerNeed = ""
+peerRepeated = ""
+
+if __name__ == '__main__':
+   FindSwappables(myNeed,myRepeated,peerNeed,peerRepeated,shinnyForShinny=True):
